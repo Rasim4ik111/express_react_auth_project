8ca326f88aac5927d8b8972d4ba1fd079f5ce3e5
Mirrasim Suleymanli <rasim95suleymanli@gmail.com>
2023-11-02 19:24:02 +0400

    Add React Router Dom

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,49 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
+import { Paths } from "./path";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: (
+      <div>
+        <Link to={Paths.home}>Home &nbsp;</Link>
+        <Link to={Paths.login}>Login &nbsp;</Link>
+        <Link to={Paths.register}>Register</Link>
+        <h1>Employees</h1>
+      </div>
+    ),
+  },
+  {
+    path: Paths.login,
+    element: (
+      <div>
+        <Link to={Paths.home}>Home &nbsp;</Link>
+        <Link to={Paths.login}>Login &nbsp;</Link>
+        <Link to={Paths.register}>Register</Link>
+        <Login />
+      </div>
+    ),
+  },
+  {
+    path: Paths.register,
+    element: (
+      <div>
+        <Link to={Paths.home}>Home &nbsp;</Link>
+        <Link to={Paths.login}>Login &nbsp;</Link>
+        <Link to={Paths.register}>Register</Link>
+        <Register />
+      </div>
+    ),
+  },
+]);
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +51,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello World</h1>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
